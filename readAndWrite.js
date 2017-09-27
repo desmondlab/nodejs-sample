c8f0bf8202f469d0b826febc6962adb1e82e286a
desmond <desmondlab@gmail.com>
2017-09-27 15:04:12 +0800

    test fs moduel

--- readAndWrite.js
+++ readAndWrite.js
@@ -1,10 +1,25 @@
 var fs = require('fs');
 
-// readFile is async (needs callback function), non-blocking code.
-fs.readFile('readMe.txt', 'utf8', function(err, data){
+// reading and writing files
+// readFileSync is sync. readFile is async (needs callback function), non-blocking code.
+fs.readFile('readMe.txt', 'utf8', function (err, data) {
     console.log("test readfile: " + data);
-    fs.writeFile('writeMe.txt', data);
+    fs.writeFile('writeMe.txt', data);  
+    //fs.unlink('writeMe.txt'); //delete file
 });
 
-// it will show first
+// it will show firstly
 console.log("test async");
+
+
+// creating directories
+fs.mkdir('stuff', function () {
+    fs.readFile('readMe.txt', 'utf8', function (err, data) {
+        fs.writeFile('./stuff/writeMe.txt', data);
+    });
+});
+
+//removing directories and files inside
+fs.unlink('./stuff/writeMe.txt', function(){
+    fs.rmdir('stuff');
+});

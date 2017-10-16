caf5e884b2c6b905817b87dec880249010e5c19a
desmond <desmondlab@gmail.com>
2017-10-16 17:45:15 +0800

    event emitter

--- testEventEmitter.js
+++ testEventEmitter.js
@@ -16,15 +16,15 @@ var ryu = new Person('ryu');
 var people = [james, mary, ryu];
 
 people.forEach(function(person){
-    // emitter.on is a node method from EventEmitter class
-    // addListener() is same as on()
-    // event listening
-    // https://nodejs.org/docs/latest/api/events.html#events_emitter_on_eventname_listener
+    //emitter.on is a node method from EventEmitter class 
+    //addListener() is same as on() 
+    //event listening 
+    //https://nodejs.org/docs/latest/api/events.html#events_emitter_on_eventname_listener
     person.on('speak', function(msg){ 
         console.log(person.name + ' said ' + msg);
     });
 });
 
-// to trigger events
+//to trigger events
 james.emit('speak', 'hey !!!!');
 ryu.emit('speak', 'what\'s up');
